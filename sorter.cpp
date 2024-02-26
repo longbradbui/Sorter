9493ee75701eecb89c0211fbc576541f7ba52a29
Hoang Long Bui <86465921+longbradbui@users.noreply.github.com>
2024-02-25 18:22:12 -0800

    Updates sorter.cpp

--- sorter.cpp
+++ sorter.cpp
@@ -1,16 +1,18 @@
 #include <vector>
 #include <string>
-//#include <sys/time.h>
+#include <sys/time.h>
 #include <iostream>
 #include "sorts.h"
 using namespace std;
+static const int kStart = 3;
+static const int kEnd = 21;
 
-/*
-//Global functions
-void InitVector(vector<int>& item_vetor, int size);
-void PrintVector(const vector<int>& item_vector, string name);
-int Elapsed(const timeval& start, const timeval& end);
-int main(int argc, char* argv[])
+// Global functions
+void InitVector(vector<int> &item_vetor, int size);
+void PrintVector(const vector<int> &item_vector, string name);
+int Elapsed(const timeval &start, const timeval &end);
+
+int main(int argc, char *argv[])
 {
 	int size = 0;
 	string sort_name = "";
@@ -49,34 +51,193 @@ int main(int argc, char* argv[])
 		PrintVector(items, string("items"));
 		cout << endl;
 	}
-	//get time to measure the time it takes to sort
+	// get time to measure the time it takes to sort
 	struct timeval start_time, end_time;
 	gettimeofday(&start_time, 0);
-	//
-	// PLACE YOUR CODE HERE
-	// ...Determine which sort to call on the vector
-	// ...The code below only looks for QuickSort
-	// Other Signatures:
-	// BubbleSort(items, 0, size - 1)
-	// InsertionSort(items, 0, size – 1)
-	// MergeSort(items, 0, size – 1)
-	// IterativeMergeSort(items, 0, size – 1)
-	// ShellSort(items, 0, size – 1)
-	// PLACE YOUR CODE HERE; for instance:
-	if (sort_name == "QuickSort")
+	/* BUBBLE SORT */
+	if (sort_name == "BubbleSort")
+	{
+		BubbleSort(items, 0, size - 1);
+		gettimeofday(&end_time, 0);
+		if (print_out)
+		{
+			cout << "Sorted:" << endl;
+			PrintVector(items, string("item"));
+		}
+		cout << "Time (microsecs): " << Elapsed(start_time, end_time) << endl;
+	}
+	/* INSERTION SORT */
+	else if (sort_name == "InsertionSort")
+	{
+		InsertionSort(items, 0, size - 1);
+		gettimeofday(&end_time, 0);
+		if (print_out)
+		{
+			cout << "Sorted:" << endl;
+			PrintVector(items, string("item"));
+		}
+		cout << "Time (microsecs): " << Elapsed(start_time, end_time) << endl;
+	}
+	/* MERGE SORT */
+	else if (sort_name == "MergeSort")
+	{
+		MergeSort(items, 0, size - 1);
+		gettimeofday(&end_time, 0);
+		if (print_out)
+		{
+			cout << "Sorted:" << endl;
+			PrintVector(items, string("item"));
+		}
+		cout << "Time (microsecs): " << Elapsed(start_time, end_time) << endl;
+	}
+	/* ITERATIVE MERGE SORT */
+	else if (sort_name == "IterativeMergeSort")
+	{
+		IterativeMergeSort(items, 0, size - 1);
+		gettimeofday(&end_time, 0);
+		if (print_out)
+		{
+			cout << "Sorted:" << endl;
+			PrintVector(items, string("item"));
+		}
+		cout << "Time (microsecs): " << Elapsed(start_time, end_time) << endl;
+	}
+	/* SHELL SORT */
+	else if (sort_name == "ShellSort")
+	{
+		ShellSort(items, 0, size - 1);
+		gettimeofday(&end_time, 0);
+		if (print_out)
+		{
+			cout << "Sorted:" << endl;
+			PrintVector(items, string("item"));
+		}
+		cout << "Time (microsecs): " << Elapsed(start_time, end_time) << endl;
+	}
+	/* QUICK SORT */
+	else if (sort_name == "QuickSort")
 	{
 		QuickSort(items, 0, size - 1);
+		gettimeofday(&end_time, 0);
+		if (print_out)
+		{
+			cout << "Sorted:" << endl;
+			PrintVector(items, string("item"));
+		}
+		cout << "Time (microsecs): " << Elapsed(start_time, end_time) << endl;
 	}
-	gettimeofday(&end_time, 0);
-	if (print_out)
-	{
-		cout << "Sorted:" << endl;
-		PrintVector(items, string("item"));
-	}
-	cout << "Time (microsecs): " << Elapsed(start_time, end_time) << endl;
 	return 0;
 }
-void InitVector(vector<int>& item_vector, int rand_max)
+
+// int main()
+// {	
+// 	/* MANUAL TESTING */
+// 	cout << "Testing Bubble Sort" << endl;
+// 	std::vector<int> arr1 = {31, 2, 17, 87, 10, 9, 0, 1, 4, 6, 5, 10, 99, 27, 33, 21, 56, 7, 87, 99, 43, 3, 71, 99, 12, 83, 2, 33, 46, 99, 22, 19, 65, 11, 97};
+// 	std::cout << "Original array: ";
+// 	for (int i = kStart; i <= kEnd; i++)
+// 	{
+// 		std::cout << arr1[i] << " ";
+// 	}
+// 	std::cout << std::endl;
+// 	BubbleSort(arr1, kStart, kEnd);
+// 	std::cout << "Sorted subrange: ";
+// 	for (int i = kStart; i <= kEnd; i++)
+// 	{
+// 		std::cout << arr1[i] << " ";
+// 	}
+// 	std::cout << std::endl;
+// 	cout << endl;
+
+// 	std::vector<int> arr2 = {31, 2, 17, 87, 10, 9, 0, 1, 4, 6, 5, 10, 99, 27, 33, 21, 56, 7, 87, 99, 43, 3, 71, 99, 12, 83, 2, 33, 46, 99, 22, 19, 65, 11, 97};
+// 	cout << "Testing Insertion Sort" << endl;
+// 	std::cout << "Original array: ";
+// 	for (int i = kStart; i <= kEnd; i++)
+// 	{
+// 		std::cout << arr2[i] << " ";
+// 	}
+// 	std::cout << std::endl;
+// 	InsertionSort(arr2, kStart, kEnd);
+// 	std::cout << "Sorted subrange: ";
+// 	for (int i = kStart; i <= kEnd; i++)
+// 	{
+// 		std::cout << arr2[i] << " ";
+// 	}
+// 	std::cout << std::endl;
+// 	cout << endl;
+
+// 	std::vector<int> arr3 = {31, 2, 17, 87, 10, 9, 0, 1, 4, 6, 5, 10, 99, 27, 33, 21, 56, 7, 87, 99, 43, 3, 71, 99, 12, 83, 2, 33, 46, 99, 22, 19, 65, 11, 97};
+// 	cout << "Testing Recursive Merge Sort" << endl;
+// 	std::cout << "Original array: ";
+// 	for (int i = kStart; i <= kEnd; i++)
+// 	{
+// 		std::cout << arr3[i] << " ";
+// 	}
+// 	std::cout << std::endl;
+// 	MergeSort(arr3, kStart, kEnd);
+// 	std::cout << "Sorted subrange: ";
+// 	for (int i = kStart; i <= kEnd; i++)
+// 	{
+// 		std::cout << arr3[i] << " ";
+// 	}
+// 	std::cout << std::endl;
+// 	cout << endl;
+
+// 	std::vector<int> arr4 = {31, 2, 17, 87, 10, 9, 0, 1, 4, 6, 5, 10, 99, 27, 33, 21, 56, 7, 87, 99, 43, 3, 71, 99, 12, 83, 2, 33, 46, 99, 22, 19, 65, 11, 97};
+// 	cout << "Testing Quick Sort" << endl;
+// 	std::cout << "Original array: ";
+// 	for (int i = kStart; i <= kEnd; i++)
+// 	{
+// 		std::cout << arr4[i] << " ";
+// 	}
+// 	std::cout << std::endl;
+// 	QuickSort(arr4, kStart, kEnd);
+// 	std::cout << "Sorted subrange: ";
+// 	for (int i = kStart; i <= kEnd; i++)
+// 	{
+// 		std::cout << arr4[i] << " ";
+// 	}
+// 	std::cout << std::endl;
+// 	cout << endl;
+
+// 	std::vector<int> arr6 = {31, 2, 17, 87, 10, 9, 0, 1, 4, 6, 5, 10, 99, 27, 33, 21, 56, 7, 87, 99, 43, 3, 71, 99, 12, 83, 2, 33, 46, 99, 22, 19, 65, 11, 97};
+// 	cout << "Testing Iterative Merge Sort" << endl;
+// 	std::cout << "Original array: ";
+// 	for (int i = kStart; i <= kEnd; i++)
+// 	{
+// 		std::cout << arr6[i] << " ";
+// 	}
+// 	std::cout << std::endl;
+// 	IterativeMergeSort(arr6, kStart, kEnd);
+// 	std::cout << "Sorted subrange: ";
+// 	for (int i = kStart; i <= kEnd; i++)
+// 	{
+// 		std::cout << arr6[i] << " ";
+// 	}
+// 	std::cout << std::endl;
+// 	cout << endl;
+
+// 	std::vector<int> arr5 = {31, 2, 17, 87, 10, 9, 0, 1, 4, 6, 5, 10, 99, 27, 33, 21, 56, 7, 87, 99, 43, 3, 71, 99, 12, 83, 2, 33, 46, 99, 22, 19, 65, 11, 97};
+// 	cout << "Testing Shell Sort" << endl;
+// 	std::cout << "Original array: ";
+// 	for (int i = kStart; i <= kEnd; i++)
+// 	{
+// 		std::cout << arr5[i] << " ";
+// 	}
+// 	std::cout << std::endl;
+// 	ShellSort(arr5, kStart, kEnd);
+// 	std::cout << "Sorted subrange: ";
+// 	for (int i = kStart; i <= kEnd; i++)
+// 	{
+// 		std::cout << arr5[i] << " ";
+// 	}
+// 	std::cout << std::endl;
+// 	cout << endl;
+
+// 	return 0;
+// }
+
+void InitVector(vector<int> &item_vector, int rand_max)
 {
 	if (rand_max < 0)
 	{
@@ -95,7 +256,7 @@ void InitVector(vector<int>& item_vector, int rand_max)
 		pool.erase(pool.begin() + spot);
 	}
 }
-void PrintVector(const vector<int>& item_vector, string name)
+void PrintVector(const vector<int> &item_vector, string name)
 {
 	int size = item_vector.size();
 	for (int i = 0; i < size; i++)
@@ -104,108 +265,7 @@ void PrintVector(const vector<int>& item_vector, string name)
 	}
 	cout << endl;
 }
-// Function to calculate elapsed time
-// Microseconds
-int Elapsed(const timeval& start, const timeval& end)
+int Elapsed(const timeval &start, const timeval &end)
 {
-	return (end.tv_sec - start.tv_sec) * 1000000
-		+ (end.tv_usec - start.tv_usec);
-}
-*/
-
-int main()
-{
-static const int kStart = 3;
-static const int kEnd = 8;
-
-	cout << "Testing Bubble Sort" << endl;
-	std::vector<int> arr1 = { 3, 2, 7, 8, 10, 9, 0, 1, 4, 6, 5 };
-	std::cout << "Original array: ";
-	for (int i = kStart; i <= kEnd; i++) {
-		std::cout << arr1[i] << " ";
-	}
-	std::cout << std::endl;
-	BubbleSort(arr1, kStart, kEnd);
-	std::cout << "Sorted subrange: ";
-	for (int i = kStart; i <= kEnd; i++) {
-		std::cout << arr1[i] << " ";
-	}
-	std::cout << std::endl;
-	cout << endl;
-
-	std::vector<int> arr2 = { 3, 2, 7, 8, 10, 9, 0, 1, 4, 6, 5 };
-	cout << "Testing Insertion Sort" << endl;
-	std::cout << "Original array: ";
-	for (int i = kStart; i <= kEnd; i++) {
-		std::cout << arr2[i] << " ";
-	}
-	std::cout << std::endl;
-	InsertionSort(arr2, kStart, kEnd);
-	std::cout << "Sorted subrange: ";
-	for (int i = kStart; i <= kEnd; i++) {
-		std::cout << arr2[i] << " ";
-	}
-	std::cout << std::endl;
-	cout << endl;
-
-	std::vector<int> arr3 = { 3, 2, 7, 8, 10, 9, 0, 1, 4, 6, 5 };
-	cout << "Testing Recursive Merge Sort" << endl;
-	std::cout << "Original array: ";
-	for (int i = kStart; i <= kEnd; i++) {
-		std::cout << arr3[i] << " ";
-	}
-	std::cout << std::endl;
-	InsertionSort(arr3, kStart, kEnd);
-	std::cout << "Sorted subrange: ";
-	for (int i = kStart; i <= kEnd; i++) {
-		std::cout << arr3[i] << " ";
-	}
-	std::cout << std::endl;
-	cout << endl;
-
-	std::vector<int> arr4 = { 3, 2, 7, 8, 10, 9, 0, 1, 4, 6, 5 };
-	cout << "Testing Quick Sort" << endl;
-	std::cout << "Original array: ";
-	for (int i = kStart; i <= kEnd; i++) {
-		std::cout << arr4[i] << " ";
-	}
-	std::cout << std::endl;
-	QuickSort(arr4, kStart, kEnd);
-	std::cout << "Sorted subrange: ";
-	for (int i = kStart; i <= kEnd; i++) {
-		std::cout << arr4[i] << " ";
-	}
-	std::cout << std::endl;
-	cout << endl;
-
-	std::vector<int> arr5 = { 3, 2, 7, 8, 10, 9, 0, 1, 4, 6, 5 };
-	cout << "Testing Shell Sort" << endl;
-	std::cout << "Original array: ";
-	for (int i = kStart; i <= kEnd; i++) {
-		std::cout << arr5[i] << " ";
-	}
-	std::cout << std::endl;
-	ShellSort(arr5, kStart, kEnd);
-	std::cout << "Sorted subrange: ";
-	for (int i = kStart; i <= kEnd; i++) {
-		std::cout << arr5[i] << " ";
-	}
-	std::cout << std::endl;
-	cout << endl;
-
-	std::vector<int> arr6 = { 3, 2, 7, 8, 10, 9, 0, 1, 4, 6, 5 };
-	cout << "Testing Iterative Merge Sort" << endl;
-	std::cout << "Original array: ";
-	for (int i = kStart; i <= kEnd; i++) {
-		std::cout << arr6[i] << " ";
-	}
-	std::cout << std::endl;
-	ShellSort(arr6, kStart, kEnd);
-	std::cout << "Sorted subrange: ";
-	for (int i = kStart; i <= kEnd; i++) {
-		std::cout << arr6[i] << " ";
-	}
-	std::cout << std::endl;
-	cout << endl;
-	return 0;
+	return (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
 }
